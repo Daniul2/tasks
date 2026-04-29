99ca565867a534de5773d124e4f86d9ebcff84d2
Daniil Medvediev <medvedievdaniil300@gmail.com>
2026-04-29 15:28:52 +0200

    18.2 Connected getTask to database

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,36 +1,45 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/tasks")
+@RequiredArgsConstructor
 public class TaskController {
+
+    private final DbService service;
+    private final TaskMapper taskMapper;
 
     @GetMapping
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+        List<Task> tasks = service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
     }
 
     @GetMapping("/{taskId}")
-    public TaskDto getTask(@PathVariable Long taskId) {
-        return new TaskDto(1L, "test title", "test_content");
+    public TaskDto getTask(@PathVariable Long taskId) throws TaskNotFoundException {
+        return taskMapper.mapToTaskDto(
+                service.getTask(taskId).orElseThrow(TaskNotFoundException::new)
+        );
     }
 
     @DeleteMapping("/{taskId}")
     public void deleteTask(@PathVariable Long taskId) {
     }
 
-    @PutMapping("/{taskId}")
-    public TaskDto updateTask(@PathVariable Long taskId, @RequestBody TaskDto taskDto) {
-        return new TaskDto(taskId, "Edited test title", "Test content");
+    @PutMapping
+    public TaskDto updateTask(@RequestBody TaskDto taskDto) {
+        return new TaskDto(taskDto.getId(), "Edited test title", "Test content");
     }
 
     @PostMapping
-    public TaskDto createTask(@RequestBody TaskDto taskDto) {
-        return new TaskDto(1L, taskDto.getTitle(), taskDto.getContent());
+    public void createTask(@RequestBody TaskDto taskDto) {
     }
 }
