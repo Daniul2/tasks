c21825b397360f9701b2d1023fac194318ce2211
Daniil Medvediev <medvedievdaniil300@gmail.com>
2026-05-26 09:39:36 +0200

    feat: refactor Trello configuration and filtering

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -1,32 +1,29 @@
 package com.crud.tasks.controller;
 
-import com.crud.tasks.domain.CreatedTrelloCard;
 import com.crud.tasks.domain.TrelloBoardDto;
-import com.crud.tasks.domain.TrelloCardDto;
-import com.crud.tasks.service.TrelloService;
+import com.crud.tasks.trello.client.TrelloClient;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/v1/trello")
+@RequestMapping("v1/trello")
 @RequiredArgsConstructor
 @CrossOrigin("*")
 public class TrelloController {
+    private final TrelloClient trelloClient;
 
-    private final TrelloService trelloService;
-
-    @GetMapping("/boards")
-    public ResponseEntity<List<TrelloBoardDto>> getTrelloBoards() {
-        return ResponseEntity.ok(trelloService.fetchTrelloBoards());
-    }
-
-    @PostMapping(value = "/cards", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<CreatedTrelloCard> createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
-        return ResponseEntity.ok(trelloService.createTrelloCard(trelloCardDto));
+    @GetMapping("boards")
+    public List<TrelloBoardDto> getTrelloBoards() {
+        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+        return trelloBoards.stream()
+                .filter(board -> board.getId() != null && board.getName() != null)
+                .filter(board -> board.getName().toLowerCase().contains("kodilla"))
+                .collect(Collectors.toList());
     }
 }
